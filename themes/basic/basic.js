61b23ab37f4de3267c30f0a48e56195229e9b76f
chilijung <chilijung@gmail.com>
2014-02-20 15:21:45 +0800

    add a basic theme

---
+++ themes/basic/basic.js
@@ -0,0 +1,26 @@
+var basic = function(errors, evt) {
+    var SELECTOR_ERRORS = $('.error_box'),
+        SELECTOR_SUCCESS = $('.success_box');
+        
+    if (errors.length > 0) {
+        SELECTOR_ERRORS.empty();
+        
+        for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+            SELECTOR_ERRORS.append(errors[i].message + '<br />');
+        }
+        
+        SELECTOR_SUCCESS.css({ display: 'none' });
+        SELECTOR_ERRORS.fadeIn(200);
+    } else {
+        SELECTOR_ERRORS.css({ display: 'none' });
+        SELECTOR_SUCCESS.fadeIn(200);
+    }
+    
+    if (evt && evt.preventDefault) {
+        evt.preventDefault();
+    } else if (event) {
+        event.returnValue = false;
+    }
+}
+
+module.exports = basic;
